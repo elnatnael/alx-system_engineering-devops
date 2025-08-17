8ff8374c3b57a6ef3d0b2b13bf04f6693ddf7fa2
elnatnael <meranat9104@gmail.com>
2025-08-18 00:52:01 +0300

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,48 @@
-# 7-puppet_install_nginx_web_server.pp
-# Installs Nginx, sets up Hello World page, and configures a 301 redirect
+# Puppet manifest to install and configure Nginx with a 301 redirect
 
-package { 'nginx':
-  ensure => installed,
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
 }
 
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  require    => Package['nginx'],
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-update'],
 }
 
 file { '/var/www/html/index.html':
   ensure  => file,
   content => 'Hello World!',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
   require => Package['nginx'],
 }
 
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+",
   require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
